16723908b01ac18d12feaef4b90383246be6d91e
Josa Vicente <jvicenpe@gmail.com>
2022-08-01 21:09:21 +0200

    Initial Commit

---
+++ WeatherAppAnimations/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeatherAppAnimations
+//
+//  Created by Josafat Vicente Pérez on 1/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAppAnimations/WeatherAppAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppAnimationsApp.swift
+//  WeatherAppAnimations
+//
+//  Created by Josafat Vicente Pérez on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
